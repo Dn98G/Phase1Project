3e5c22f229f9c5df225fecacd6729e155152332f
Denis Cheruiyot <Cheruiyotkp974@gmail.com>
2025-03-31 23:25:38 +0300

    project

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const listContainer = document.getElementById("list"); //selects the list id to display footbal clubs
+  const listContainer = document.getElementById("list");
 
-  const badge = document.getElementById("badge"); //selects the element id'd as badge
+  const badge = document.getElementById("badge");
 
-  const description = document.getElementById("description"); //selects description element
-  const name = document.getElementById("name"); //selects name element
+  const description = document.getElementById("description");
+  const name = document.getElementById("name");
   const league = document.getElementById("league");
   const position = document.getElementById("position");
   const captain = document.getElementById("captain");
@@ -14,40 +14,38 @@ document.addEventListener("DOMContentLoaded", () => {
   const players = document.getElementById("players");
   const stadium = document.getElementById("stadium");
 
-  // Fetch clubs from db.json file
+  
   fetch("db.json")
-    .then((response) => response.json()) //converts response to objects
+    .then((response) => response.json()) 
     .then((data) => {
-      const clubs = data.clubs; //fetches data
-      displayClubs(clubs); //club data to be displayed
+      const clubs = data.clubs; 
+      displayClubs(clubs); 
     })
     .catch((error) => console.error("Error fetching data:", error));
 
   document.getElementById("explore").addEventListener("click", () => {
-    document.getElementById("list").scrollIntoView({ behavior: "smooth" }); //to make my club list scroll into view when the explore button is clicked
+    document.getElementById("list").scrollIntoView({ behavior: "smooth" }); /
   });
 
   function displayClubs(clubs) {
-    // Function to display clubs list
-    listContainer.innerHTML = ""; //clears the list before adding a new list
+    
+    listContainer.innerHTML = "";
 
     clubs.forEach((club) => {
-      //for each loops through each club
-      const clubItem = document.createElement("li"); //creates a new li element
-      clubItem.textContent = club.name; //introduces club name into the list
-      clubItem.classList.add("club-item"); //adds a class for styling
+      const clubItem = document.createElement("li"); 
+      clubItem.textContent = club.name; 
+      clubItem.classList.add("club-item");
 
-      clubItem.setAttribute("data-league", club.league); //sets league name to data attribute in order to filter the clubs easily
+      clubItem.setAttribute("data-league", club.league);
 
-      clubItem.addEventListener("click", () => displayDetails(club)); //function to show details
+      clubItem.addEventListener("click", () => displayDetails(club));
 
-      listContainer.appendChild(clubItem); //adds a new li item to club list
+      listContainer.appendChild(clubItem);
     });
   }
 
-  // Function to display club details
   function displayDetails(club) {
-    badge.src = club.badge; //sets the text contents to their club properties in the objects
+    badge.src = club.badge; 
     name.textContent = club.name;
     description.textContent = club.description;
     league.textContent = club.league;
@@ -63,28 +61,24 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function searchClubs() {
-  const searchInput = document.getElementById("search").value; //gets the input value from id #search
+  const searchInput = document.getElementById("search").value; 
 
   document.querySelectorAll(".club-item").forEach((clubItem) => {
-    //selects all club elements and compares if the values match with the searched item
-    clubItem.style.display = clubItem.textContent.includes(searchInput) //I used a ternary operator instead of if/else because it is shorter,,,,I used .style to easily change visibility without CSS
-      ? "block" //if yes it is displayed     //I  also used .include instead of === to make it live-search friendly
-      : "none"; //if not it is hidden
+    clubItem.style.display = clubItem.textContent.includes(searchInput) 
+      ? "block" 
+      : "none"; 
   });
 }
-
-document.getElementById("search").addEventListener("input", searchClubs); //calls searchClubs every time there  is an input
+document.getElementById("search").addEventListener("input", searchClubs);
 
 function filter() {
-  const league = document.getElementById("filter").value; //gets the selected league from #filter
+  const league = document.getElementById("filter").value; 
   document.querySelectorAll(".club-item").forEach((clubItem) => {
-    //loops through all clubs
-    const clubLeague = clubItem.getAttribute("data-league"); //gets each clubs league using the attribute value I set earlier
+    const clubLeague = clubItem.getAttribute("data-league");
 
     clubItem.style.display =
-      league === "All" || clubLeague === league ? "block" : "none"; //If the selected league is "All" it shows all clubs,,,,otherwise it shows the clubs matching selected leagues
+      league === "All" || clubLeague === league ? "block" : "none";
   });
 }
 
-document.getElementById("filter").addEventListener("change", filter); //I tried adding an event listener to "league" in line 80 but it did not work,,then I did some research and found out that its because its not a DOM element
-//we add an event listener to filter because we want to know when a user selects a new league,,therefore we are listening to "change" event
+document.getElementById("filter").addEventListener("change", filter);
